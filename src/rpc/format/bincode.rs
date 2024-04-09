2fa1c230e45de97fba4d375e2ea74ff21eccb232
Raphael Darley <raphael@raphaeldarley.com>
2024-04-09 09:40:36 +0200

    wip

--- src/rpc/format/bincode.rs
+++ src/rpc/format/bincode.rs
@@ -13,18 +13,25 @@ use surrealdb::sql::Value;
 
 pub fn req_ws(msg: Message) -> Result<Request, RpcError> {
 	match msg {
-		Message::Binary(val) => {
-			deserialize::<Value>(&val).map_err(|_| RpcError::ParseError)?.try_into()
-		}
+		Message::Binary(val) => req(&val),
 		_ => Err(RpcError::InvalidRequest),
 	}
 }
 
-pub fn res_ws(res: Response) -> Result<(usize, Message), RpcError> {
+pub fn res_ws(res_val: Response) -> Result<(usize, Message), RpcError> {
 	// Serialize the response with full internal type information
-	let res = surrealdb::sql::serde::serialize(&res).unwrap();
+	let res_bytes = res(res_val.into()).unwrap();
 	// Return the message length, and message as binary
-	Ok((res.len(), Message::Binary(res)))
+	Ok((res_bytes.len(), Message::Binary(res_bytes)))
+}
+
+pub fn req(val: &[u8]) -> Result<Request, RpcError> {
+	deserialize::<Value>(val).map_err(|_| RpcError::ParseError)?.try_into()
+}
+
+pub fn res(res: Value) -> Result<Vec<u8>, RpcError> {
+	// Serialize the response with full internal type information
+	Ok(surrealdb::sql::serde::serialize(&res).unwrap())
 }
 
 pub fn req_http(val: &Bytes) -> Result<Request, RpcError> {
